949475319d5e40a9e15ef4c834aaf3725c7a33df
Gabriel28 <nasc.gabriel.1997@gmail.com>
2024-09-13 03:24:43 -0300

    Added new params  to send the payment gateway

--- checkout/src/services/checkoutService.js
+++ checkout/src/services/checkoutService.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PAYMENT_GATEWAY_URL = process.env.PAYMENT_GATEWAY_URL || 'http://payment-gateway:8082/payment';
-
+const PAYMENT_GATEWAY_URL = process.env.PAYMENT_GATEWAY_URL
 class CheckoutService {
-    static processPayment = async (orderId, amount) => {
+    static processPayment = async (orderId, paymentId, card, amount) => {
         const paymentRequest = {
             orderId: orderId,
+            paymentId: paymentId,
+            card: card,
             amount: amount
         };
 
         try {
             const response = await axios.post(PAYMENT_GATEWAY_URL, paymentRequest);
+            console.log(response.data)
             return response.data.success ? { status: 'SUCCESS' } : { status: 'CANCELLED' }
         } catch (error) {
             return { status: 'CANCELLED' };
