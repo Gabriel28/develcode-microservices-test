775a9cc0ed1c7be8c3751cdcbe2ae2044df495eb
Gabriel28 <nasc.gabriel.1997@gmail.com>
2024-09-13 03:18:57 -0300

    Create the Java Payment Gateway

---
+++ payment_gateway/src/main/java/com/gabriel28/develcodetest/payment_gateway/controller/PaymentController.java
@@ -0,0 +1,4 @@
+package com.gabriel28.develcodetest.payment_gateway.controller;
+
+public class PaymentController {
+}
---
+++ payment_gateway/src/main/java/com/gabriel28/develcodetest/payment_gateway/model/PaymentResponse.java
@@ -0,0 +1,4 @@
+package com.gabriel28.develcodetest.payment_gateway.model;
+
+public class PaymentResponse {
+}
