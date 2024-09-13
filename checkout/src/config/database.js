96cb179f95ca4b24c3b92fdd5ca5983c384ffe50
Gabriel28 <nasc.gabriel.1997@gmail.com>
2024-09-13 03:22:27 -0300

    Add dotenv config and change const vlaues to process.env

--- checkout/src/config/database.js
+++ checkout/src/config/database.js
@@ -1,7 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('checkout', 'root', 'secret', {
-    host: 'db',
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE, 
+    'root',
+    process.env.MYSQL_ROOT_PASSWORD, {
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql'
 });
 
